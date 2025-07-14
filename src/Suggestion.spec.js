dc8a8a52eda8f372e50687a55bdb4031ae00a190
Youngbum Kim <kimuyb@gmail.com>
2025-07-14 16:58:27 +0900

    Remove test and build code

--- src/Suggestion.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Suggestion', () => {
-  it.todo('should be possible to pass a focussed style.')
-  it.todo('should render the current display of the suggestion.')
-  it.todo(
-    'should highlight the part of the display that is matched by the current query.'
-  )
-})
